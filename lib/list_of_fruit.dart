a76d3482b68f0f1bcbf316b69c45e60cbf348128
Vishal Keshri <vishalkeshri858@gmail.com>
2024-03-29 13:24:47 +0530

    29-03-2024

---
+++ lib/list_of_fruit.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.menu,color: Colors.white),
+          actions: [Icon(Icons.notifications_active,color: Colors.white,)],
+          backgroundColor: Colors.teal,
+          centerTitle: true,
+          title:
+          Text('🛍️List of Fruits', style: TextStyle(color: Colors.white)),
+        ),
+        body: const Center(
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: '🍎 Apple',
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '\n🍇 Graps',
+                  style: TextStyle(
+                      color: Colors.purpleAccent,
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '\n🍒 Cherry',
+                  style: TextStyle(
+                      color: Colors.deepPurpleAccent,
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '\n🍓 Strawberry',
+                  style: TextStyle(
+                      color: Colors.pinkAccent,
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '\n🥭 Mango',
+                  style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '\n🍍 Pineapple',
+                  style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '\n🍋 Lemon',
+                  style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '\n🍉 Watermelon',
+                  style: TextStyle(
+                      color: Colors.greenAccent,
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '\n🥥 Coconut',
+                  style: TextStyle(
+                      color: Colors.brown,
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
